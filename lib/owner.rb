4dedaf7d8620ac066de6849264d4e8398598c70e
Joy Lin <vvlnote@hotmail.com>
2019-03-27 00:20:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -81,7 +81,7 @@ class Owner
   
   def list_pets
 
-    str = "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)"
+    str = "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
  
   end
 end
